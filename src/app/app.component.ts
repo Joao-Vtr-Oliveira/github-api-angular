d202bbfd4b5ae9cf259c7c914ade087b35634f3d
João V. <joao.vtr.oliveira@outlook.com>
2025-06-18 11:41:02 -0300

    chores: remove app css file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,6 @@ import { UserComponent } from './user/user.component';
   selector: 'app-root',
   imports: [UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
 export class AppComponent {
   

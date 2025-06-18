9a117de904fb2b44420263b97bad68bce1143f73
João V. <joao.vtr.oliveira@outlook.com>
2025-06-18 08:56:55 -0300

    feat: create the user component and some global styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

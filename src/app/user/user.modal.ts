158cbb80f55cf6a6dda6714c300ebe008c302c2f
João V. <joao.vtr.oliveira@outlook.com>
2025-06-18 09:00:02 -0300

    feat: create user model

---
+++ src/app/user/user.modal.ts
@@ -0,0 +1,14 @@
+export interface userType {
+  avatar_url: string;
+  html_url: string;
+  repos_url: string;
+  name: string;
+  company: string | null;
+  location: string | null;
+  email: string | null;
+  hirable: boolean;
+  bio: string;
+  followers: number;
+  following: number;
+  created_at: string;
+}
